accbce3f665b3c7bf7c9366255553fe4fba5b5a3
danielasakpa <danielasakpa@gmail.com>
2023-12-09 09:26:43 +0100

    first commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -116,7 +116,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="background-img"></img>
     </section>
   )
 }
